6aee62d624ccdb83034410f492abd9f3c2a87a0e
Gaminggul <41211227+Gaminggul@users.noreply.github.com>
2025-10-12 01:28:56 +0200

    middleware.ts fixed

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,24 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
-export function middleware(req: NextRequest) {
-  const host = req.headers.get('host') || '';
+export function middleware(request: NextRequest) {
+  const host = request.headers.get('host') || ''
+
   if (host.startsWith('test.')) {
-    const res = NextResponse.next();
-    res.headers.set('X-Robots-Tag', 'noindex, nofollow');
-    return res;
+    const res = NextResponse.next()
+    res.headers.set('X-Robots-Tag', 'noindex, nofollow')
+    return res
   }
-  return NextResponse.next();
+
+  if (host === 'www.fajanzen.de') {
+    const url = request.nextUrl
+    url.host = 'fajanzen.de'
+    return NextResponse.redirect(url, 301)
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/:path*'],
 }
